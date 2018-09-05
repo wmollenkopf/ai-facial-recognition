baddd3512f95606b2389ffcc6639ef0717bed042
wmollenkopf <biri@biri.me>
2018-09-04 20:33:57 -0400

    * Added react-particles-js * Updated some CSS to help make particles nested in the background.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ImageSubmissionForm from './components/ImageSubmissionForm/ImageSubmissionForm';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 876
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Particles className='particles'
+                params={particlesOptions}
+              />
+        <div>Hello World</div>
         <FaceRecognition />
         <ImageSubmissionForm />
       </div>
